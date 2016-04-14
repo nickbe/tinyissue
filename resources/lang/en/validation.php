4f7ac6fb0da48d5594c18053815bf2b296b22bcf
nickbe <marvin@core42.com>
2016-04-14 18:21:23 +0200

    Add status to user and adapt login
    Users have now a status flag and can be enabled/disabled. The login
    takes the status into account and login screen has a new look.

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -102,5 +102,6 @@ return [
         'only_complete_if_changing_password' => 'Only complete if changing password',
         'remember_me'                        => 'Remember me?',
         'kanban_board'                       => 'Kanban board',
+		'extended_user_settings'             => 'Extended user settings',
     ],
 ];
